1ba426ec3e7c9850ff36792755b117e9408bb424
Augusto Gabriel Rodrigues dos Santos <augusto.santos@dataeasy.com.br>
2023-11-20 16:56:40 -0300

    Atualização na logica de apagar heroi e ver historico dos mesmos

--- models.py
+++ models.py
@@ -1,9 +1,8 @@
 # models.py
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, Boolean
 from sqlalchemy.orm import declarative_base
-from database import Base, engine
-
-
+from database import Base
+from datetime import datetime
 Base = declarative_base()
 
 class Herois(Base):
@@ -12,8 +11,9 @@ class Herois(Base):
     nome = Column(String(150))
     superpoder = Column(String(150))
     raca = Column(String(150))
+    data_entrada = Column(Date)
+    data_saida = Column(Date)
+    ativo = Column(Boolean, default=True)
 
     def __repr__(self):
-        return '<herois %r>' % (self.id)
-# models.py
-
+        return f'<Herois(id={self.id}, nome={self.nome}, superpoder={self.superpoder}, raca={self.raca}, data_entrada={self.data_entrada}, data_saida={self.data_saida}, ativo={self.ativo})>'
